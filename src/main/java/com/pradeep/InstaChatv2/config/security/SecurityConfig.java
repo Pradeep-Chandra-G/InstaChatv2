b973153adb4a0b13be0c9817a1e4f5c4d38ceca4
Pradeep-Chandra-17 <94434721+Coder-Pradeep-999@users.noreply.github.com>
2024-11-26 22:00:33 +0530

    changed

--- src/main/java/com/pradeep/InstaChatv2/config/security/SecurityConfig.java
+++ src/main/java/com/pradeep/InstaChatv2/config/security/SecurityConfig.java
@@ -47,6 +47,7 @@ public class SecurityConfig {
                 .formLogin(form -> form
                         .loginPage("/login")
                         .permitAll()
+                        .defaultSuccessUrl("/")
                 ) // Defaults for form login (login page is automatically set)
                 .logout(logout -> logout
                         .invalidateHttpSession(true)
